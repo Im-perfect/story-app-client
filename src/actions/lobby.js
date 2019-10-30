13cb375ec4ccf94151408710d4825b0ee644ca85
Im-perfect <zhangke.kerri@gmail.com>
2019-10-30 13:59:19 +0100

    get random with dummy data

--- src/actions/lobby.js
+++ src/actions/lobby.js
@@ -10,35 +10,55 @@ export const addLobby = (gameInfo, playerJWT) => (dispatch, getState) => {
     .set("playerJWT", playerJWT)
     .send(gameInfo)
     .then(res => {
-        console.log("res.body", res.body)
+      console.log("res.body", res.body);
       dispatch({
         type: ADD_LOBBY,
         payload: res.body
       });
-      console.log("body id", res.body.id)
-      return res.body.id
+      console.log("body id", res.body.id);
+      return res.body.id;
     })
-    .catch(console.error); 
+    .catch(console.error);
 };
 
 export const join = (gameId, playerJWT) => (dispatch, getState) => {
-    //post request => response.id etc.
-    return request
-      .put(`${baseUrl}/lobbies/${gameId}`)
-      .set("playerJWT", playerJWT)
-      .then(res => {
-          console.log("res.body", res.body)
-        dispatch({
-          type: ADD_LOBBY,
-          payload: res.body
-        });
-        console.log("body id", res.body.id)
-        return res.body.id
-      })
-      .catch(console.error); 
-  };
+  //post request => response.id etc.
+  return request
+    .put(`${baseUrl}/lobbies/${gameId}`)
+    .set("playerJWT", playerJWT)
+    .then(res => {
+      console.log("res.body", res.body);
+      dispatch({
+        type: ADD_LOBBY,
+        payload: res.body
+      });
+      console.log("body id", res.body.id);
+      return res.body.id;
+    })
+    .catch(console.error);
+};
 
 export const addLobbies = payload => ({
   type: "ADD_LOBBIES",
   payload
 });
+
+export const fillRandom = () => (dispatch, getState) => { 
+    const a = {
+            place: "random",
+            character: "random",
+            verb: "random",
+            noun: "random"
+        }
+        return  a
+//   return request
+//     .get(`${baseUrl}/description/random`)
+//     .then(res => {
+//         // dispatch({
+//         //     type: "GET_RANDOM_DESC",
+//         //     description: res.body
+//         // })
+//        //res.body
+//     })
+//     .catch(console.error);
+};
