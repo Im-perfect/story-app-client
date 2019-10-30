e17ac41a4e61fe289d5ff8ead7ba90f2a2cdd669
Im-perfect <zhangke.kerri@gmail.com>
2019-10-30 15:51:07 +0100

    set up game info stream

--- src/components/GameContainer.js
+++ src/components/GameContainer.js
@@ -1,44 +1,52 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import {getGame} from '../actions/game'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getGame } from "../actions/game";
+import { baseUrl } from "../actions/url";
 
 class GameContainer extends Component {
-    state = {
-        startGame: false
-    }
+  state = {
+    startGame: false
+  };
 
-    //source = new EventSource(`${url}/game/:id/stream`); //setup the stream!!!
+  source = new EventSource(`${baseUrl}/lobbies/${this.props.match.params.id}`); //setup the stream!!!
 
-    componentDidMount(){
-        const gameId = this.props.match.params.id
-        this.props.getGame(gameId)
+  componentDidMount() {
+    const gameId = this.props.match.params.id;
+    this.props.getGame(gameId);
 
-        // this.source.onmessage = event => {
-        //     //console.log("got a message", event);
-        //     const messages = JSON.parse(event.data);
-        //     this.setState({
-        //       messages
-        //     });
-        //     this.props.addMessages(messages);
-        //   };
-    }
+    this.source.onmessage = event => {
+      const game = JSON.parse(event.data);
+      console.log("GAME", game);
+      this.setState({
+        startGame: game.player1 && game.player2 ? true : false
+      });
+      //this.props.addMessages(messages);
+    };
+  }
 
-    render() {
-        if (!this.state.startGame) return "Waiting for another player..."
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  render() {
+    const player1 = this.props.currentGame.player1 || "New Player";
+    const player2 = this.props.currentGame.player2 || "New Player";
+
+    return (
+      <div>
+        {!this.state.startGame
+          ? "Waiting for another player..."
+          : `Game: ${player1} with ${player2}`}
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = (state) => ({
-    
-})
+const mapStateToProps = state => ({
+  currentGame: state.currentGame
+});
 
 const mapDispatchToProps = {
-    getGame
-}
+  getGame
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(GameContainer)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(GameContainer);
